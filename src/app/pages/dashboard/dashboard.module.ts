93d5dc7194b41b2844269e1de6b0a2966c82adc7
Mittul <conanmittul@gmail.com>
2021-02-11 05:32:48 +0530

    banner added in the dashboard

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { BannerComponent } from './banner/banner.component';
 
 
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, BannerComponent],
   imports: [
     CommonModule,
     DashboardRoutingModule
